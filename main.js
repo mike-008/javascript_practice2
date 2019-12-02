93490af3f54ba02e43f41e3e7c609d67845ba52a
mike <neko60077@gmail.com>
2019-12-02 17:40:35 +0900

    課題修正の質問をお願いします。

--- main.js
+++ main.js
@@ -1,109 +1,113 @@
 'use strict'
 {
     // 要素の取得
-    const ADDBUTTON = document.getElementById('addbutton');
-    const TBODY = document.getElementById('tbody');
+    const addButton = document.getElementById('addbutton');
+    const tbody = document.getElementById('tbody');
+    const allRadiobutton = document.getElementById('all_radiobutton');
+    const workingRadiobutton = document.getElementById('working_radiobutton');
+    const completeRadiobutton = document.getElementById('complete_radiobutton');
     
-    const TASKLISTS = [];
+    const tr = document.createElement('tr');
+    const tdId = document.createElement('td');
+    const tdComment = document.createElement('td');
+    const statusButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
     
+    const tasklists = [];
+    
+    // todoリストのタスクを非表示にする処理
+    function hidden () {
+        tdId.classList.add('hidden');
+        tdComment.classList.add('hidden');
+        statusButton.classList.add('hidden');
+        deleteButton.classList.add('hidden');
+    }
+    
+    // todoリストのタスクを表示させる処理
+    function removeHidden () {
+        tdId.classList.remove('hidden');
+        tdComment.classList.remove('hidden');
+        statusButton.classList.remove('hidden');
+        deleteButton.classList.remove('hidden');
+    }
+
     // ☆追加ボタンが押された際の処理
-    ADDBUTTON.addEventListener('click', e => {
+    addButton.addEventListener('click', e => {
         e.preventDefault();
         
-        // 入力された値（task）を取得、空の配列（TASKLISTS）に追加
-        const TASK = document.getElementById('task').value;
-        TASKLISTS.push(TASK);
+        // 入力された値（task）を取得、空の配列（tasklists）に追加
+        const task = document.getElementById('task').value;
+        tasklists.push(task);
         
-        while (TBODY.firstChild) {
-            TBODY.removeChild(TBODY.firstChild);
+        while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
         }  
         
         // 追加された値とインデックス番号を取り出し,出力する
-        TASKLISTS.forEach((toDo, index) => {
-            const TR = document.createElement('tr');
-            const TDID = document.createElement('td');
-            const TDCOMMENT = document.createElement('td');
-            const STATUSBUTTON = document.createElement('button');
-            const DELETEBUTTON = document.createElement('button');
+        tasklists.forEach((toDo, index) => {
+            
 
-            TDID.textContent =` ${index}`;
-            TDCOMMENT.textContent = `${toDo}`;
-            STATUSBUTTON.textContent = '作業中';
-            DELETEBUTTON.textContent ='削除';
+            tdId.textContent =` ${index}`;
+            tdComment.textContent = `${toDo}`;
+            statusButton.textContent = '作業中';
+            deleteButton.textContent ='削除';
 
-            TBODY.appendChild(TDID);
-            TBODY.appendChild(TDCOMMENT);
-            TBODY.appendChild(STATUSBUTTON);
-            TBODY.appendChild(DELETEBUTTON);
-            TBODY.appendChild(TR);
+            tbody.appendChild(tdId);
+            tbody.appendChild(tdComment);
+            tbody.appendChild(statusButton);
+            tbody.appendChild(deleteButton);
+            tbody.appendChild(tr);
+        });//tasklists.forEach((toDo
             
             // ☆作業中/完了ボタンを押した時の処理
-            STATUSBUTTON.addEventListener('click', e => {
+            statusButton.addEventListener('click', e => {
                 e.preventDefault();
 
-                if (STATUSBUTTON.textContent === '作業中') {
-                    STATUSBUTTON.textContent = '完了';
+                if (statusButton.textContent === '作業中') {
+                    statusButton.textContent = '完了';
 
-                } else if (STATUSBUTTON.textContent === '完了') {
-                    STATUSBUTTON.textContent = '作業中';
+                } else if (statusButton.textContent === '完了') {
+                    statusButton.textContent = '作業中';
                 };
             });
             
             // ☆削除ボタンを押した時の処理
-            DELETEBUTTON.addEventListener('click', () => {
+            deleteButton.addEventListener('click', () => {
 
-                TDID.parentNode.removeChild(TDID);
-                TDCOMMENT.parentNode.removeChild(TDCOMMENT);
-                STATUSBUTTON.parentNode.removeChild(STATUSBUTTON);
-                DELETEBUTTON.parentNode.removeChild(DELETEBUTTON);
+                tdId.parentNode.removeChild(tdId);
+                tdComment.parentNode.removeChild(tdComment);
+                statusButton.parentNode.removeChild(statusButton);
+                deleteButton.parentNode.removeChild(deleteButton);
             });
             
-            // ☆ラジオボタンをチェックした時の処理
-            const ALL_RADIOBUTTON = document.getElementById('all_radiobutton');
-            const WORKING_RADIOBUTTON = document.getElementById('working_radiobutton');
-            const COMPLETE_RADIOBUTTON = document.getElementById('complete_radiobutton');
-
-            function hidden () {
-                TDID.classList.add('hidden');
-                TDCOMMENT.classList.add('hidden');
-                STATUSBUTTON.classList.add('hidden');
-                DELETEBUTTON.classList.add('hidden');
-            }
             
-            function removeHidden () {
-                TDID.classList.remove('hidden');
-                TDCOMMENT.classList.remove('hidden');
-                STATUSBUTTON.classList.remove('hidden');
-                DELETEBUTTON.classList.remove('hidden');
-            }
-            // ☆すべてをチェックした時の処理
-            ALL_RADIOBUTTON.addEventListener('change', () => {
-                if (STATUSBUTTON.textContent === '完了' || STATUSBUTTON.textContent ==='作業中') {
-                    removeHidden();
-                }
-            });
-            // ☆作業中をチェックした時の処理
-            WORKING_RADIOBUTTON.addEventListener('change', () => {
-                removeHidden();
 
-                if (STATUSBUTTON.textContent === '完了') {
-                    hidden();
-                };
-            });
-            // ☆完了をチェックした時の処理
-            COMPLETE_RADIOBUTTON.addEventListener('change', () => {
-                removeHidden();
-
-                if (STATUSBUTTON.textContent === '作業中') {
-                    hidden();
-                }
-            });
-
-        });//TASKLISTS.forEach((toDo
         
-    });//ADDBUTTON.addEventListener('click'
+        
+    });//addButton.addEventListener('click'
 
-    
+    // ☆すべてをチェックした時の処理
+    allRadiobutton.addEventListener('change', () => {
+        if (statusButton.textContent === '完了' || statusButton.textContent ==='作業中') {
+            removeHidden();
+        }
+    });
+    // ☆作業中をチェックした時の処理
+    workingRadiobutton.addEventListener('change', () => {
+        removeHidden();
+
+        if (statusButton.textContent === '完了') {
+            hidden();
+        };
+    });
+    // ☆完了をチェックした時の処理
+    completeRadiobutton.addEventListener('change', () => {
+        removeHidden();
+
+        if (statusButton.textContent === '作業中') {
+            hidden();
+        }
+    });    
     
         
  
